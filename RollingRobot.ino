25dddd16b9fab9f534ad08702cb4bb59c86239a9
Iban <iban.fernandez@laposte.net>
2019-02-12 23:39:37 +0100

    Encoders code added.

--- RollingRobot.ino
+++ RollingRobot.ino
@@ -1,11 +1,41 @@
+//*** LIBRARIES ***
 #include <AutoPID.h>
+#include "RotaryIncrementalEncoder.h"
 
+// Initialize static members of class RotaryIncrementalEncoder.
+// This step is needed because the RotaryIncrementalEncoder uses interrupts with static
+// methods and members.
+int16_t RotaryIncrementalEncoder::encoderRotationRightCounter;
+int16_t RotaryIncrementalEncoder::encoderRotationLeftCounter;
+uint16_t RotaryIncrementalEncoder::encoderSpeedRightCounter;
+uint16_t RotaryIncrementalEncoder::encoderSpeedLeftCounter;
+bool RotaryIncrementalEncoder::flagMeasureSpeed;
+bool RotaryIncrementalEncoder::flagMeasureRotation;
+
+//*** OBJECTS DECLARATIONS ***
+RotaryIncrementalEncoder* Encoder1;
+RotaryIncrementalEncoder* Encoder2;
+
+//*** SETUP ***
 void setup() {
-  // put your setup code here, to run once:
+  // Serial communicaiton with PC
+  Serial.begin(9600);
+  Serial.println("Start programm");
 
+  // Objects instanciations
+  Encoder1 = new RotaryIncrementalEncoder(1);
+  Encoder2 = new RotaryIncrementalEncoder(2);
+  
+  Encoder1->StartSpeedMeasurement();
+  Encoder2->StartSpeedMeasurement();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print(Encoder1->GetSpeed());
+  Serial.print(", ");
+  Serial.print(Encoder2->GetSpeed());
+  Serial.println();
+
+  delay(100);
 
 }
